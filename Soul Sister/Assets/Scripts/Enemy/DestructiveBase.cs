289e76ee96a6d7dd781ba0b83534d8fd1d642e34
Ihago Freire Santos <ihagog12@gmail.com>
2018-01-17 00:36:57 -0300

    Inicio Criação scripts

---
+++ Soul Sister/Assets/Scripts/Enemy/DestructiveBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DestructiveBase : MonoBehaviour {
+
+	public float currentLife;
+
+	// Use this for initialization
+	protected void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		
+	}
+
+	public void applyDamage(int damage){
+
+		currentLife-=damage;
+
+		if(currentLife <= 0){
+			OnDestroyed();
+		}
+	}
+
+	public abstract void OnDestroyed();
+}
