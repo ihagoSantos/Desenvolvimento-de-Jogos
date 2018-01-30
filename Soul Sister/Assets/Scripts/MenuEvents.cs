9739d5444a20bc09d5eca3557428151ee1d3ba66
Ihago Freire Santos <ihagog12@gmail.com>
2018-01-30 00:20:44 -0300

    Add audio no menu

---
+++ Soul Sister/Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEvents : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void newGameEvent(){
+		Application.LoadLevel(1);
+	}
+
+	public void loadEvent(){
+
+	}
+
+	public void creditsEvent(){
+
+	}
+
+}
