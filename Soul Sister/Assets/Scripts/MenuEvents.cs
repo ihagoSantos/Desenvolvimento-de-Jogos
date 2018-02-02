ea5bf517138d53b7768b181cff6e545a022806a1
Ihago Freire Santos <ihagog12@gmail.com>
2018-02-02 18:36:11 -0300

    Hierarquia de Menus

--- Soul Sister/Assets/Scripts/MenuEvents.cs
+++ Soul Sister/Assets/Scripts/MenuEvents.cs
@@ -25,19 +25,24 @@ public class MenuEvents : MonoBehaviour {
 	public void newGameEvent(){
 		
 		source.PlayOneShot(efeitoClick);
-		Application.LoadLevel(1);
+		Application.LoadLevel(2);
 	
 	}
 
 	public void loadEvent(){
 
 		source.PlayOneShot(efeitoClick);
-		Application.LoadLevel(2);
+		Application.LoadLevel(1);
 	
 	}
 
 	public void creditsEvent(){
-
+		source.PlayOneShot(efeitoClick);
+		Application.LoadLevel(4);
+	}
+	public void backToMenuEvent(){
+		source.PlayOneShot(efeitoClick);
+		Application.LoadLevel(0);
 	}
 
 }
