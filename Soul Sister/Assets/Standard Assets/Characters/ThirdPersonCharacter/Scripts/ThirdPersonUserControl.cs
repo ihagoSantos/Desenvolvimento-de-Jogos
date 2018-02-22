024dc850770fd55e77a4d8b568022422aba0d6b6
Ihago Freire Santos <ihagog12@gmail.com>
2018-02-22 00:16:48 -0300

    Personagem

--- Soul Sister/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Soul Sister/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -14,8 +14,13 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
 
         
+		private AnimationController animationController;
+
         private void Start()
         {
+
+			animationController = GetComponent<AnimationController> ();
+
             // get the transform of the main camera
             if (Camera.main != null)
             {
@@ -30,11 +35,14 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
             // get the third person character ( this should never be null due to require component )
             m_Character = GetComponent<ThirdPersonCharacter>();
+
+
         }
 
 
         private void Update()
         {
+			
             if (!m_Jump)
             {
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
@@ -45,6 +53,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         // Fixed update is called in sync with physics
         private void FixedUpdate()
         {
+			
+
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
@@ -56,20 +66,28 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 // calculate camera relative direction to move:
                 m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
                 m_Move = v*m_CamForward + h*m_Cam.right;
+				//animationController.PlayAnimation (AnimationState.IDLE);
             }
             else
             {
                 // we use world-relative directions in the case of no main camera
                 m_Move = v*Vector3.forward + h*Vector3.right;
+
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+			if (Input.GetKey(KeyCode.LeftShift)){
+				m_Move *= 0.5f;
+				//animationController.PlayAnimation (AnimationState.WALK);
+			}
 #endif
 
             // pass all parameters to the character control script
+
             m_Character.Move(m_Move, crouch, m_Jump);
             m_Jump = false;
+
         }
+
     }
 }
