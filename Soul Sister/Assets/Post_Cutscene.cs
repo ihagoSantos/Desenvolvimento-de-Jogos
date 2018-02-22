f40543ad90536c040c369819a8d7a3c53620d676
Lejes <franciscolejes@gmail.com>
2018-02-22 00:51:26 -0300

    Cutscene and Toon Shader

---
+++ Soul Sister/Assets/Post_Cutscene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Post_Cutscene : MonoBehaviour {
+
+	public float tempoAtual ;
+	public float tempoParar ;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.tempoAtual += 1f;
+		if(this.tempoAtual == this.tempoParar){
+			Application.LoadLevel(2);
+		}
+	}
+
+}
